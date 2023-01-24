97324e637dc995567be9ddeaf765f3fb161535b8
Lucas Pereira <lucaspereiradf07@gmail.com>
2023-01-24 20:22:36 -0300

    Category model created

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,13 +2,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Library.Models;
 
-public class Author
+public class Category
 {
     [Key]
     public int Id { get; set; }
-    
+
     [Required(ErrorMessage = "Campo Requerido")]
     [MaxLength(80, ErrorMessage = "Este campo deve conter entre 3 e 80 caracteres")]
     [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 80 caracteres")]
-    public string? Name { get; set; }
+    public string? Name { get; set;}
 }
